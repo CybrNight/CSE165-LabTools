af7c8bf09b99b53bc1c4bd8f6ca39960059d5b73
Nathan Estrada <naestrada19@gmail.com>
2022-02-04 19:06:06 -0800

    Updated install script

--- install.sh
+++ install.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-#wget -O - https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master/install.sh | sudo bash
+#sudo wget -O - https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master/install.sh | sudo bash
+
+#Define colors
+black="$(tput setaf 0)"
+red="$(tput setaf 9)" 
+green="$(tput setaf 10)"
+yellow="$(tput setaf 11)" 
+blue="$(tput setaf 12)"
+purple="$(tput setaf 13)" 
+cyan="$(tput setaf 14)" 
+white="$(tput setaf 15)" 
+bold=$(tput bold) 
+RESET=$(tput reset) 
+
+echo Hello
 
 #Define GitHub root
 source=https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master
  
-install(){
-    mkdir resource
-    wget -O resource/t.cpp $source/resource/lab_gen.sh
-    wget -O resource/t_std.cpp $source/resource/lab_gen.sh
-    wget -O lab_gen.sh $source/lab_gen.sh
-}
-
-update(){
-    rm -r resource
-    install
-}
-
-while getopts 'iur' flag;
-do
-case $flag in
-    i) install ;;
-    u) update ;;
-    *) printf "Usage: ./install.sh -i | -u" ;;
-    esac
-done   
+mkdir resource
+wget -qO resource/t.cpp $source/resource/t.cpp
+wget -qO resource/t_std.cpp $source/resource/t_std.cpp
+wget -qO lab_gen.sh $source/lab_gen.sh

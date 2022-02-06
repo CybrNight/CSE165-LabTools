02b657617d37947953e360f59c46a2cea2b5e659
Nathan Estrada <naestrada19@gmail.com>
2022-02-05 22:21:51 -0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,6 @@
 #Define GitHub root
 source=https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master
 
-if [ -d "LabTools" ] ; then
-exit
-fi
-
-#Define colors
-black="$(tput setaf 0)"
-red="$(tput setaf 9)" 
-green="$(tput setaf 10)"
-yellow="$(tput setaf 11)" 
-blue="$(tput setaf 12)"
-purple="$(tput setaf 13)" 
-cyan="$(tput setaf 14)" 
-white="$(tput setaf 15)" 
-bold=$(tput bold) 
-RESET=$(tput reset) 
-
 git clone https://github.com/CybrNight/CSE165-LabTools.git
 cd CSE165-LabTools
 

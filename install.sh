1394a90914c4cfad61d10684d6d1d92626402c60
Nathan Estrada <naestrada19@gmail.com>
2022-02-05 21:55:22 -0800

    Added install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#sudo wget -qO - https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master/install.sh | sudo bash
+
+#Define GitHub root
+source=https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master
+
+if [ -d "LabTools" ] ; then
+exit
+fi
+
+#Define colors
+black="$(tput setaf 0)"
+red="$(tput setaf 9)" 
+green="$(tput setaf 10)"
+yellow="$(tput setaf 11)" 
+blue="$(tput setaf 12)"
+purple="$(tput setaf 13)" 
+cyan="$(tput setaf 14)" 
+white="$(tput setaf 15)" 
+bold=$(tput bold) 
+RESET=$(tput reset) 
+
+git clone https://github.com/CybrNight/CSE165-LabTools.git
+cd CSE165-LabTools
+make
+cp bin/app ..

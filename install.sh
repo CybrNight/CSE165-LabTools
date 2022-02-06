2560b4190f72f8e9a66685c545e4db7175ff09d2
Nathan Estrada <naestrada19@gmail.com>
2022-02-06 00:03:42 -0800

    More changes

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #sudo wget -qO - https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master/install.sh | sudo bash
 
-
 #Define GitHub root
 source=https://raw.githubusercontent.com/CybrNight/CSE165-LabTools/master
 
-git clone https://github.com/CybrNight/CSE165-LabTools.git
-
-make CSE165-LabTools/Makefile
-cp CSE165-LabTools/bin/lab_tools lab_tools
+mkdir CSE165-LabTools
+git clone https://github.com/CybrNight/CSE165-LabTools.git temp
+ 
+make temp/Makefile
+cp /temp/bin/lab_tools CSE165-LabTools/lab_tools
+cp -r /temp/res/ /CSE165-LabTools
 
 echo Finished building!
 rm -r CSE165-LabTools
 echo Removed source files!
 echo Finished!
-echo Run lab_tools binary.
+echo Run lab_tools binary in this directory
